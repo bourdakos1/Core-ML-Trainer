551de9eca9624c6e942ef111db07b6de20387a76
Nick Bourdakos <bourdakos1@gmail.com>
2018-04-11 02:14:35 -0400

    classifier name bug

--- new/Visual Recognition/TrainViewController.swift
+++ new/Visual Recognition/TrainViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 import CoreData
 
 class TrainViewController: UIViewController {
-    @IBOutlet var modelName: UITextField!
+    @IBOutlet var modelName: UITextField! {
+        didSet {
+            if classifier.name != "Untitled Model" {
+                modelName.text = classifier.name
+            }
+        }
+    }
     
     var classifier = PendingClassifier()
     
@@ -33,9 +39,16 @@ class TrainViewController: UIViewController {
         alert.view.addSubview(loadingIndicator)
         present(alert, animated: true, completion: nil)
         
+        if let text = modelName.text, text.isEmpty {
+            classifier.name = "Untitled Model"
+        } else if let text = modelName.text {
+            classifier.name = text
+        }
+        
+        DatabaseController.saveContext()
+        
         // Not sure if this needs to be weak or unowned. You shouldn't be able to leave the page so we can probably leave it as is...?
         // Lets do weak to be safe
-        classifier.name = modelName.text!
         classifier.train(completion: { [weak self] response in
             guard let `self` = self else { return }
             self.dismiss(animated: false, completion: {
@@ -45,6 +58,14 @@ class TrainViewController: UIViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let text = modelName.text, text.isEmpty {
+            classifier.name = "Untitled Model"
+        } else if let text = modelName.text {
+            classifier.name = text
+        }
+        
+        DatabaseController.saveContext()
+        
         if  segue.identifier == "additionalClasses",
             let destination = segue.destination as? SnapperViewController {
             
